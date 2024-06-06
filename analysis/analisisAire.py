57a4b2e88ef193bb85e9a397b1740421ba8b0496
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-06-05 20:33:39 -0500

    se crean filtros

--- analysis/analisisAire.py
+++ analysis/analisisAire.py
@@ -8,7 +8,24 @@ def construirAireDataFrame():
     #generamos el dataframe
     aireDataFrame=pd.DataFrame(datosAire,columns=['nombre','comuna','ica','fecha','correo'])
 
-    crearTabla(aireDataFrame,"datosaire")
-    print(aireDataFrame)
+    #crearTabla(aireDataFrame,"datosaire")
+    #Limpiando el dataframe
+    #reemplazando valores 
+    aireDataFrame.replace('sin',pd.NA,inplace=True)
+    #elimino los registros que no cumplen el criterio
+    aireDataFrame.dropna(inplace=True)
+    
+    #filtrar DATOS
+    #Filtrar es aplicar condiciones logicas
+    #que permitan analizar la informacion del DF
+    filtroCalidadAireBueno=aireDataFrame.query("(ica>=10)and(ica<40)").value_counts()
+    filtroCalidadAireAceptable=aireDataFrame.query("(ica>=40)and(ica<50)").value_counts()
+    filtroCalidadAireMalo=aireDataFrame.query("(ica>=50)and(ica<100)").value_counts()
+
+    print(filtroCalidadAireBueno)
+    print("\n")
+    print(filtroCalidadAireAceptable)
+    print("\n")
+    print(filtroCalidadAireMalo)
 
 construirAireDataFrame()
--- data/generators/generadorAire.py
+++ data/generators/generadorAire.py
@@ -7,7 +7,7 @@ def generarDatosCalidadAire():
         nombre=random.choice(['ana perez','jose jimeno','marco polo','martha lucrecia','karen andrea'])
         comuna=random.randint(1,14)
         ica=random.randint(10,80)
-        fecha=random.choice(['2024-05-15','2024-05-16','2024-05-17'])
+        fecha=random.choice(['2024-05-15','2024-05-16','2024-05-17','sin'])
         correo=random.choice(['correo@correo.com','correo2@correo.com','correo3@correo.com','4@correo.com','5@correo.com'])
 
         encuesta=[nombre,comuna,ica,fecha,correo]
